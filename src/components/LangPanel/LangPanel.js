4b12ad1ddb7d2e09c456a8d57d01e946946a5033
unknown <samolazov.roman.alex@gmail.com>
2024-02-22 17:30:17 +0300

    ADD dipatch for buttons

--- src/components/LangPanel/LangPanel.js
+++ src/components/LangPanel/LangPanel.js
@@ -1,14 +1,19 @@
+import { useDispatch } from 'react-redux';
+
+import {getEngTxt, getRuTxt} from '../../store/slices/langSlice';
+
 import './LangPanel.scss';
 
 const LangPanel = () => {
+    const dispatch = useDispatch();
     return (
         <div className="lang-panel">
             <ul className='lang-panel-items flex-r'>
                 <li className='lang-panel_item'>
-                    <button id='btn-RU' className='btn-lang' >Рус</button>
+                    <button id='btn-RU' className='btn-lang' onClick={() => dispatch(getRuTxt())}>Рус</button>
                 </li>
                 <li>
-                    <button id='btn-EN' className='btn-lang' >Eng</button>
+                    <button id='btn-EN' className='btn-lang' onClick={() => dispatch(getEngTxt())}>Eng</button>
                 </li>
             </ul>
         </div>
