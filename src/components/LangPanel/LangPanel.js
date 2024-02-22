039e7ea05d7ef467e1b0c513e56c2ffb23c47c3f
unknown <samolazov.roman.alex@gmail.com>
2024-02-22 16:10:34 +0300

    ADD lang-panel with styles

--- src/components/LangPanel/LangPanel.js
+++ src/components/LangPanel/LangPanel.js
@@ -2,8 +2,15 @@ import './LangPanel.scss';
 
 const LangPanel = () => {
     return (
-        <div>
-            LangPanel
+        <div className="lang-panel">
+            <ul className='lang-panel-items flex-r'>
+                <li className='lang-panel_item'>
+                    <button id='btn-RU' className='btn-lang' >Рус</button>
+                </li>
+                <li>
+                    <button id='btn-EN' className='btn-lang' >Eng</button>
+                </li>
+            </ul>
         </div>
     );
 };
