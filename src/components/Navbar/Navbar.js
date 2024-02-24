917465baf455b48722cd43ad87abf4a8f30060db
unknown <samolazov.roman.alex@gmail.com>
2024-02-25 00:19:11 +0300

    add navbar with style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,22 @@
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import './Navbar.scss';
 
 const Navbar = () => {
+    const text = useSelector((state) => state.lang.text.header.navbar);
     return (
-        <div>
-            navbar
-        </div>
+        <nav className='app-nav'>
+            <ul className='app-nav-items flex-r'>
+                {
+                    text.map((elm, idx) => (
+                        <li className='app-nav__item' key={idx}>
+                            <NavLink to={elm[1]}>{elm[0]}</NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
     );
 };
 
